5955b3d5e7685ebf4a270f2f0d3e217002c190fa
root <magnifisaintama@gmail.com>
2022-09-09 03:38:59 -0700

    tasks4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main - printing
+ * 
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{	
+	puts("\""Programming is like building a multilingual puzzle");
+        return(0);
+}
