aed3ed4073356e3cdfcf88a3de6bcd253dbe3150
root <magnifisaintama@gmail.com>
2022-09-09 07:00:24 -0700

    task put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - printing
+ * main - Entry point
  * 
  * Return: Always 0 (successful)
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-        return(0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
