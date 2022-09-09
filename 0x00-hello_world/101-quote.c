9e97e6658324616b2b08dd612a262a2124d6afa1
root <magnifisaintama@gmail.com>
2022-09-09 06:19:38 -0700

    task101 update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,12 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- *
- *
- * Return: Always 1 (Success)
- *
+ * return: Always 1 (success)
  */
-
 int main(void)
-
 {
-
-write(2, "and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n", 59);
-return (1);
-
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
 }
