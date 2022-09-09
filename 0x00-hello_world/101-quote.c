c426dd9f8ea8fef9ba66408120c9d0442229243e
root <magnifisaintama@gmail.com>
2022-09-09 06:43:33 -0700

    tasks101 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
+ * decription: quoting
  *
  * return: Always 1 (success)
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
