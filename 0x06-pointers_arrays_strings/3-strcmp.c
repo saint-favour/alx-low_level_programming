bf95369935735dce04436a6640dda97743dd086c
root <magnifisaintama@gmail.com>
2022-09-22 14:42:05 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: if str1 < str2, the negative differrnce of the first unmatched cha.
+ * Return: if str1 < str2, the negative differrnce of the first unmatche.
  * if strl == str2, 0.
- * if strl > str1 > str2, the positve differences of the first unmatched cha.
+ * if strl > str1 > str2, the positve differences of the first unmatched.
  */
 int _strcap(char *s1, char *s2)
 {
