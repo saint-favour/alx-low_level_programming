ed886889102ac7e07c988d37d642a1eef54c520a
saint-favour <magnifisaintama@gmail.com>
2022-10-03 23:52:44 +0100

    corrections

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,36 +8,36 @@
 
 char *argstostr(int ac, char **av)
 {
-    char *str;
-    int count = 0, a = 0, b = 0, c = 0;
+char *str;
+int count = 0, a = 0, b = 0, c = 0;
 
-    if (ac == 0 || av == NULL)
-    return(NULL);
-    while (a < ac)
-    {
-        b = 0;
-        while (av[a][b] != '\0')
-        {
-            count++;
-            b++;
-        }
-        a++;
-    }
-    count = count + ac + 1;
-    str = malloc(sizeof(char) * count);
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    for (a = 0; a < ac; a++)
-    {
-        for (b = 0; av[a][b] != '\0'; b++)
-        {
-            str[c] = av[a][b];
-            c++;
-        }
-        str[c] = '\n';
-        c++;
-    }
-    return (str);
+if (ac == 0 || av == NULL)
+return(NULL);
+while (a < ac)
+{
+b = 0;
+while (av[a][b] != '\0')
+{
+count++;
+b++;
+}
+a++;
+}
+count = count + ac + 1;
+str = malloc(sizeof(char) * count);
+if (str == NULL)
+{
+return (NULL);
+}
+for (a = 0; a < ac; a++)
+{
+for (b = 0; av[a][b] != '\0'; b++)
+{
+str[c] = av[a][b];
+c++;
+}
+str[c] = '\n';
+c++;
+}
+return (str);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,15 +7,15 @@
 
 int _strlen(char *s)
 {
-    unsigned int i;
+unsigned int i;
 
-    i = 0;
-    while (s[i] != '\0') 
-    {
-        i++;
-    }
+i = 0;
+while (s[i] != '\0') 
+{
+i++;
+}
 
-    return (i);
+return (i);
 }
 
 /**
@@ -26,31 +26,31 @@ int _strlen(char *s)
 */
  char *str_concat(char *s1, char *s2)
  {
-    char *dst;
-    unsigned int 1, j, size;
+char *dst;
+unsigned int 1, j, size;
 
-    if (s1 == NULL)
-    s1 = '';
+if (s1 == NULL)
+s1 = '';
 
-    if (s2 == NULL)
-    s2 = '';
+if (s2 == NULL)
+s2 = '';
 
-    size = (_strlen(s1) + _strlen(s2) + 1);
+size = (_strlen(s1) + _strlen(s2) + 1);
 
-    dst = (char *) malloc(size * sizeof(char));
+dst = (char *) malloc(size * sizeof(char));
 
-    if (dst == 0)
-    {
-        return (NULL);
-    }
-    for (i = 0; *(s1 + i) != '\0'; i++)
-    *(dst + 1) = *(s1 + i);
+if (dst == 0)
+{
+return (NULL);
+}
+for (i = 0; *(s1 + i) != '\0'; i++)
+*(dst + 1) = *(s1 + i);
 
-    for (j = 0; *(s2 + j) != '\0'; j++)
-    {
-    *(dst + 1) = *(s2 + j);
-    i++;
-    }
+for (j = 0; *(s2 + j) != '\0'; j++)
+{
+*(dst + 1) = *(s2 + j);
+i++;
+}
 
-    return (dst)
- }
+return (dst)
+}
